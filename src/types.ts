7e9c4a1238253c91256ebd90eda30feec118e8f2
Kavindu Santhusa <89186091+ksenginew@users.noreply.github.com>
2021-09-21 20:52:49 +0530

    Delete src directory

--- src/types.ts
+++
@@ -1,9 +0,0 @@
-import * as CSS from 'csstype';
-
-export interface Properties extends CSS.PropertiesHyphenFallback<string | number, string | number> {
-    [key: string]: string | number | (string | number | undefined)[] | Properties | undefined
-}
-export type Pseudos = { [P in CSS.SimplePseudos]: Properties }
-export interface Style extends Pseudos {
-    [key: string]: Properties
-}
