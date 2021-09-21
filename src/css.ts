5b5f1dea789c0ddaa587243b70809d8650c9b34a
Kavindu Santhusa <89186091+ksenginew@users.noreply.github.com>
2021-09-21 18:22:12 +0530

    Update css.ts

--- src/css.ts
+++ src/css.ts
@@ -10,14 +10,33 @@ let cache: { [key: string]: string } = {}
 /**
  * css entry
  */
-export function css(css: Style) {
+function css(css: Style, global?:boolean, global?:keyframes) {
+    let styles = 
     // Get a string representation of the object that is called 'compiled'
     // Retrieve the className from cache or hash it in place
     let className = toHash(JSON.stringify(css));
 
     // If there's no entry for the current className
     if (!cache[className]) {
-        parse(css, '.' + className).forEach(rule => (style.sheet as CSSStyleSheet).insertRule(rule))
+        parse(
+            // For keyframes
+            keyframes ? { ['@keyframes ' + className]: css } : css,
+            global ? '' : '.' + className
+        ).forEach(rule => (style.sheet as CSSStyleSheet).insertRule(rule))
     }
     return className
 }
+
+/**
+ * CSS Global function to declare global styles
+ * @type {Function}
+ */
+let glob = (styles: Style)=> css(styles, !0);
+
+/**
+ * `keyframes` function for defining animations
+ * @type {Function}
+ */
+let keyframes = (styles: Style)=> css(styles, !1, !0);
+
+export { css, glob, keyframes };
