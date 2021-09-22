2b7535702966488df56ee11e22dea26743e36588
Kavindu Santhusa <89186091+ksenginew@users.noreply.github.com>
2021-09-22 07:48:07 +0530

    Create css.ts

---
+++ src/css.ts
@@ -0,0 +1,40 @@
+import type { Style} from './types'
+import  { cache} from './cache'
+import  { toHash} from './hash'
+import  { parse} from './parse'
+
+/**
+ * css entry
+ */
+function css(css: Style, global?: boolean, keyframes?: boolean) {
+    let stringified = JSON.stringify(css);
+    // Get a string representation of the object that is called 'compiled'
+    // Retrieve the className from cache or hash it in place
+    let className = toHash(stringified);
+
+    // If there's no entry for the current className
+    if (cache.indexOf(stringified) == -1) {
+        parse(
+            // For keyframes
+            keyframes ? { ['@keyframes ' + className]: css } : css,
+            global ? '' : '.' + className
+        ).forEach((rule) => sheet.insertRule(rule));
+        cache.push(stringified);
+    }
+
+    return className;
+}
+
+/**
+ * CSS Global function to declare global styles
+ * @type {Function}
+ */
+let glob = (styles: Style) => css(styles, !0);
+
+/**
+ * `keyframes` function for defining animations
+ * @type {Function}
+ */
+let keyframes = (styles: Style) => css(styles, !1, !0);
+
+export { css, glob, keyframes };
