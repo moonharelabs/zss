7315698af0864ae262904223d929a050057bc621
Kavindu Santhusa <89186091+ksenginew@users.noreply.github.com>
2021-09-22 07:42:17 +0530

    Create sheet.ts

---
+++ src/sheet.ts
@@ -0,0 +1,9 @@
+let sheet: { insertRule: ((rule: string) => number) } =
+    typeof window == 'object'
+        ? (
+              Object.assign(
+                  document.head.appendChild(document.createElement('style')),
+                  { innerHTMl: ' ' }
+              ).sheet as CSSStyleSheet
+          )
+        : {insertRule(rule: string) {return ssr.push(rule)}}
